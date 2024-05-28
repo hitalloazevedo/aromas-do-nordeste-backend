4f3df1368ee68c21217b98f3b3d9b4adedfccc55
Hitallo Azevedo <azevedohitallo@gmail.com>
2024-05-28 20:19:49 -0300

    feat: função deletar prato e aplicando boas praticas no response

--- index.js
+++ index.js
@@ -5,15 +5,28 @@ const app = express()
 app.use(express.json())
 const port = 3000
 
-app.post('/cadastrar', async (req, res) => {
+app.post('/cardapio', async (req, res) => {
     const { name, description, imageUrl} = req.body;
-    const msg = await controller.insertPlate(name, description, imageUrl)
-    res.status(201).send(msg)
+    const response = await controller.insertPlate(name, description, imageUrl)
+    if (response.code == 201){
+        res.status(response.code).send(response.msg)
+    } else {
+        res.status(response.code).send(response.msg)
+    }
 })
 
 app.get('/cardapio', async (req, res) => {
     const data = await controller.getAllPlates()
     res.status(200).send(data)
+})
+
+app.delete('/cardapio/:id', async(req, res) => {
+    const response = await controller.deletePlate(req.param('id'))
+    if (response.code == 202){
+        res.status(response.code).send(response.msg)
+    } else {
+        res.status(response.code).send(response.msg)
+    }
 })
 
 app.listen(port, () => {
