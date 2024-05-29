16395e18e11f0f56651805d74f52223b089b2b80
Hitallo Azevedo <azevedohitallo@gmail.com>
2024-05-29 10:04:08 -0300

    refactor: alterando de sequelize para supabase

--- connection.js
+++ connection.js
@@ -1,47 +1,11 @@
-import { Sequelize, DataTypes} from 'sequelize'
+import { createClient } from "@supabase/supabase-js"; 
+import { configDotenv } from "dotenv";
 
-const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: './database/pratos.sqlite'
-})
+configDotenv()
 
-async function openConnection(){
-    try{
-        await sequelize.authenticate();
-        const Plate = sequelize.define(
-            'Plates',
-            {
-                id: {
-                    type: DataTypes.INTEGER,
-                    allowNull: false,
-                    primaryKey: true,
-                    autoIncrement: true,
-                    unique: true,
-                },
-                name: {
-                    type: DataTypes.STRING,
-                    allowNull: false,
-                },
-                description : {
-                    type: DataTypes.STRING(500),
-                    allowNull: false,
-                },
-                imageUrl: {
-                    type: DataTypes.STRING,
-                    allowNull: false,
-                },
-            },
-            {
-                freezeTableName: true,
-            },
-        )
-    
-        Plate.sync()
-        return Plate
+const supabase = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_KEY
+)
 
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
-}
-
-export default openConnection
+export default supabase;
