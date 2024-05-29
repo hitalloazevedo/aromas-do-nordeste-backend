d5ad8fc606ef189f4ebb5226bef713a9b25b6397
Hitallo Azevedo <azevedohitallo@gmail.com>
2024-05-29 00:22:28 -0300

    fix: adicionando cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import controller from './controller.js'
+import cors from 'cors'
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 const port = 3000
 
 app.post('/cardapio', async (req, res) => {
